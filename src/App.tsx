817e5ec7a5bb87dad28736207565db6b7dfa6a0d
crishard <crislantorres9@gmail.com>
2025-01-21 22:26:05 -0300

    Configurando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { Toaster } from "react-hot-toast"
+import RecipeManager from "./components/RecipeManager"
 
+const queryClient = new QueryClient()
 
 function App() {
-  
-
   return (
-    <>
-      <h2>oi</h2>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RecipeManager />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster position="top-right" />
+    </QueryClientProvider>
   )
 }
 
 export default App
+
